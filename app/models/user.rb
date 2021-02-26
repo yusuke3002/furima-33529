4d6147c67ce1f7e0fcfbda7d9723b3959607cd9a
yusuke3002 <sy.ws.ik.108@gmail.com>
2021-02-26 17:46:48 +0900

    userモデルのテストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :nickname, :last_name, :first_name, :last_name_ruby, :first_name_ruby, :birth, presence: true
+  with_options presence: true do
+    validates :nickname
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: 'is invalid Input full-width characters'} do
+      validates :last_name
+      validates :first_name
+    end
+    with_options format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid Input full-width katakana characters'} do
+      validates :last_name_ruby
+      validates :first_name_ruby
+    end
+    validates :birth
+  end
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Please include both letters and numbers'
+  validates :email, uniqueness: true
 end
