2eeaaa895fd26792ddc73964d4209f49ac4e4b68
yusuke3002 <sy.ws.ik.108@gmail.com>
2021-03-08 18:12:37 +0900

    FactoryBotの修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :item do
     item_name          {'テスト画像'}
     description        {'テスト画像です'}
-    category_id        {'2'}
-    state_id           {'2'}
-    delivery_charge_id {'2'}
-    prefecture_id      {'2'}
-    delivery_days_id   {'2'}
-    price              {'1000'}
+    category_id        { 2 }
+    state_id           { 2 }
+    delivery_charge_id { 2 }
+    prefecture_id      { 2 }
+    delivery_days_id   { 2 }
+    price              { 1000 }
     association :user
     after(:build) do |item|
       item.image.attach(io: File.open('spec/fixtures/test_image.png'), filename: 'test_image.png', content_type: 'image/png')
