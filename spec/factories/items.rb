d95596c6479e701c84497d69103d1832f5a4ecbe
yusuke3002 <sy.ws.ik.108@gmail.com>
2021-03-08 15:46:45 +0900

    itemモデルのテストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name          {'テスト画像'}
+    description        {'テスト画像です'}
+    category_id        {'2'}
+    state_id           {'2'}
+    delivery_charge_id {'2'}
+    prefecture_id      {'2'}
+    delivery_days_id   {'2'}
+    price              {'1000'}
+    association :user
+    after(:build) do |item|
+      item.image.attach(io: File.open('spec/fixtures/test_image.png'), filename: 'test_image.png', content_type: 'image/png')
+    end
   end
 end
