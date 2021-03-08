d7ce1e22b4f48c1506c7d1415a28fab08df01300
yusuke3002 <sy.ws.ik.108@gmail.com>
2021-03-08 16:36:43 +0900

    item_price.jsファイルの移動

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,7 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
-require("../channels/item_price");
+require("../item_price");
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
